63689183e9c35c81fd7d981be0830c754c4ec188
Serhii Taran <serg.taran1970@gmail.com>
2025-08-06 21:58:17 +0300

    add-tasks-local-storage

---
+++ js/feedback.js
@@ -0,0 +1,40 @@
+const STORAGE_KEY = 'feedback-msg';
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
+
+populateTextArea();
+
+// - Скасовуємо стандартну поведінку
+// - Видаляємо повідомлення зі сховища
+// - Очищаємо форму
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  console.log('submitting the form');
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+// - Отримуємо значення поля
+// - Зберігаємо його у сховище
+
+function handleInput(event) {
+  const message = event.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+// - Отримуємо значення зі сховища
+// - Якщо там щось бeло, оновлюємо DOM
+
+function populateTextArea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    textarea.value = savedMessage;
+  }
+}
